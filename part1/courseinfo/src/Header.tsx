bf5da58e32da438d4921250dd1058d352d20a2c0
Banele <mkhizeb174@gmail.com>
2025-07-07 11:01:05 +0200

    react app for exercise structure

---
+++ part1/courseinfo/src/Header.tsx
@@ -0,0 +1,11 @@
+
+const Header = (props)=>{
+    const course = 'Half Stack application development'
+    return(
+        <>
+        <h1>{props.course}</h1>
+        </>
+    )
+}
+
+export default Header;
